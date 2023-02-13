b1b1c84f6a0af1677b88162710d85d645c0d9c64
carlmnm <carlossmiguell@gmail.com>
2023-02-13 15:02:08 -0300

    feat: CRUD clients

--- src/controller/clientController.js
+++ src/controller/clientController.js
@@ -14,9 +14,9 @@ export async function getCustomersById(req, res) {
     try {
         const customer = await db.query(`SELECT * FROM customers WHERE id = $1;`, [id])
         if (!customer) {
-            return res.status(200)
+            return res.status(404)
         }
-        res.send(customer.rows)
+        res.status(200).send(customer.rows)
     } catch (error) {
         res.status(500).send(error.message)
     }
